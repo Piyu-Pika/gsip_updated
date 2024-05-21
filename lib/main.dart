4a321deafb076d7564dc4485da7f34fba8fc85f7
Piyush <piyushbhardwaj1603@gmail.com>
2024-05-21 17:24:44 +0530

    darkmode

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
       home: LoginDemo(),
     );
   }
